a57007b257128a4890225722c7ae93f811ee2f50
emmaedem <emmaontop86@yahoo.com>
2023-03-06 18:55:58 -0800

    update

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = "Hol"
+word_last_2 = "on"
+middle_word = "ber"
+print(f"first 3 letters: {word_first_3}")
+print(f"last 2 letters: {word_last_2}")
+print(f"middle word: {middle_word}")
