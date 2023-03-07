124fe833ef5e032054a1b6366203f7f298b98b07
emmaedem <emmaontop86@yahoo.com>
2023-03-06 19:10:23 -0800

    update

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "school"
-# To print out the string: welcome to Holberton schoolt
-print(f"welcome to {str1} {str2}!")
-
+str1 += "" + str2
+print("welcome to {}!".format(str1))
