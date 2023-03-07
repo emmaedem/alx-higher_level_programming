02fa9418c2cc0520ce0352524115afa0b43a1686
emmaedem <emmaontop86@yahoo.com>
2023-03-06 19:34:12 -0800

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "pyhton is an interpreted, interactive, object-oriented programming\
 lanuage that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] +str[:6]
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
