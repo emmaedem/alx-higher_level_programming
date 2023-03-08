eb3652ff6d2c8268137e9c0a25a37d209ab983ec
emmaedem <emmaontop86@yahoo.com>
2023-03-08 09:07:23 -0800

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = "pyhton is an interpreted, interactive, object-oriented programming\
+str = "python is an interpreted, interactive, object-oriented programming\
 lanuage that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]:
-    print(str)
+str = str[39:67] + str[107:112] + str[:6]
+print("object-oriented programming with Python\n")
+
