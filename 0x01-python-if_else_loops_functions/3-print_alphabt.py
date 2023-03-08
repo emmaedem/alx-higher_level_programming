31c690f0a3b1d2a71374d7ee7664d5c57f37c671
emmaedem <emmaontop86@yahoo.com>
2023-03-07 17:43:10 -0800

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,5 @@
 # Print all the letters except q and e.
 
 for alphabet in range(97, 123):
-	if alphabet != 101 and alphabet != 113:
-		print(chr(alphabet), end="")
+    if alphabet != 101 and alphabet != 113:
+        print(chr(alphabet), end="")
