ca42ea1d0b9f222b1c8b6dc2a1850819978ff831
emmaedem <emmaontop86@yahoo.com>
2023-03-08 12:53:48 -0800

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,4 @@
 # a program that prints numbers from 0 to 99.
 
 for i in range(0, 100):
-    if i == 99:
-        print("{}".format(i))
-    else:
-        print("{:02}".format(i), end=", ")
+    print("{:02}".format(i), end= ", ")
