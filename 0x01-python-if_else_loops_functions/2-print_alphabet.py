0c17b5d667a041fdfeb46dd6867ddcde06a842c7
emmaedem <emmaontop86@yahoo.com>
2023-03-07 17:45:22 -0800

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 #a program that prints the ASCII alphabet, in lowercase, not followed by a new line.
 
 for alpha in range(97, 123):
-	print("{}".format(chr(alpha)), end="")
+    print("{}".format(chr(alpha)), end="")
