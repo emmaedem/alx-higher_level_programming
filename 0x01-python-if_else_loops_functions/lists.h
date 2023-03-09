309d723ace95f8f78b8596dcf65d699289848be8
emmaedem <emmaontop86@yahoo.com>
2023-03-08 17:15:11 -0800

    update

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,26 +1,49 @@
-#ifndef LISTS_H
-#define LISTS_H
-
+#include "lists.h"
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
+ * insert_node - function that inserts number into a sorted singly linked list
+ * @head: head of the linked list
+ * @number: data of the new node
+ * Return: address of new node or NULL if fails
  */
-
-typedef struct listint_s
+listint_t *insert_node(listint_t **head, int number)
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	listint_t *new, *aux1 = *head, *aux2 = *head, *aux3 = *head;
+	unsigned int i = 0, idx = 0;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-listint_t *insert_node(listint_t **head, int number);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
 
-#endif /* LISTS_H */
+	if (*head == NULL)
+	{	new->next = *head;
+		*head = new;
+		return (new);	}
+
+	while (aux1->next)
+	{	idx++;
+		aux3 = aux3->next;
+		if (aux1->n > number)
+		{	idx--;
+			break;	}
+		else if (aux1->next->next == NULL && aux3->n < number)
+		{	idx++;
+			break;	}
+		aux1 = aux1->next;	}
+	if (idx == 0)
+	{	new->next = *head;
+		*head = new;
+		return (new);	}
+	while (aux2 != NULL && i < idx - 1)
+	{	aux2 = aux2->next;
+		i++;	}
+	if (i == idx - 1)
+	{	new->next = aux2->next;
+		aux2->next = new;
+		return (new);
+	}
+	return (NULL);
+}
