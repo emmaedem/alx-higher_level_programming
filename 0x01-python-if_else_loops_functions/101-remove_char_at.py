6c4f585fc7aa2a4502e4485072b6002851f6f181
emmaedem <emmaontop86@yahoo.com>
2023-03-14 11:36:12 -0700

    update

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 # a program that prints the ASCII alphabet, in reverse order, 
 # alternating lowercase and uppercase (z in lowercase and Y in uppercase),
 # not followed by a new line.
