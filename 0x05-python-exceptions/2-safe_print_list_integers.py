7963e82e28b42b7660cf6f5c1769e030d0d1016f
emmaedem <emmaontop86@yahoo.com>
2023-03-27 15:24:49 -0700

    update

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    for i in my_list:
+
+        try:
+
+            if count == x:
+
+                break
+
+            if isinstance(i, int):
+
+                print("{:d}".format(i), end=" ")
+
+                count += 1
+
+        except ValueError:
+
+            pass
+
+    print()
+
+    return count
