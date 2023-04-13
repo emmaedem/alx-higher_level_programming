febd5c23c6273d05686cdf46a5ba4045de525b8c
emmaedem <emmaontop86@yahoo.com>
2023-04-12 18:13:39 -0700

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
+"""Defines a text file reading function"""
 
 def read_file(filename=""):
+
     """"Reads text file and print it to stdout"""
-    with open(filename, "r", encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+
+    with open(filename, 'r', encoding="utf-8") as f:
+
+            print(f.read(), end="")
