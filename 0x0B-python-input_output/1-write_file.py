29146ceb26ccf3ec1c88c7df12bfa2b00f3f9a4e
emmaedem <emmaontop86@yahoo.com>
2023-04-12 19:20:05 -0700

    update

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""write_file module"""
+
+def write_file(filename="", text=""):
+    """write string to a text file"""
+
+    wrote = 0
+
+    with open(filename, "w", encoding="utf-8") as f:
+
+        wrote = f.write(text)
+
+    return wrote
